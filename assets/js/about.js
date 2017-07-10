3f65d5688b7facef8b41b32da0ac6b5fc705d6f5
Jônatas F. Vieira <jonatasvieira@gmail.com>
2017-07-10 03:00:25 -0300

    Added - HTML, CSS and JS files from "About" page.

---
+++ assets/js/about.js
@@ -0,0 +1,33 @@
+// Set SCROLLSPY to the SUBNAVBAR object
+$('body').scrollspy({ target: '#subnavbar', offset: 190 }); 
+
+// Custom EM and SPAN.SIDE-INFO heights
+$(".personas").each(function(){
+  var em = [], sideInfo = [];
+
+  $(this).find(".person em").each(function(){
+    em.push($(this).height());
+  });
+  $(this).find(".person .side-info").each(function(){
+    sideInfo.push($(this).height());
+  });
+
+  em = Math.max.apply(null, em);
+  sideInfo = Math.max.apply(null, sideInfo);
+
+  $(this).find(".person em").each(function(){
+    $(this).height(em+"px")
+  });
+  $(this).find(".person .side-info").each(function(){
+    $(this).height(sideInfo+"px")
+  });
+
+});
+
+// Sets the right height fro each vertical line under each year
+$(".timeline").each(function(){
+  $(this).height( $(this).parent().next().height() - $(this).prev().height() );
+});
+
+// Sets the right position for the SUB-NAVBAR
+$("#subnavbar").data("offset-top", $("#subnavbar").offset().top - ($("body > nav").height() + 20)).css("top",$("body > nav").height());
